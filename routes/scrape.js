04e1fbfa054d057e76dbf3eb889969a5d3d6a8b2
alexjamesmx <sg.alex32@gmail.com>
2024-08-29 16:48:09 -0600

    update openai key

--- routes/scrape.js
+++ routes/scrape.js
@@ -206,7 +206,7 @@ module.exports = (context) => {
       }
     }
   });
-
+  //
   router.get("/history", async (req, res) => {
     try {
       const token = req.headers.authorization.split(" ")[1];
